84002bdc8b5de2d88367fe3e02ab12e7d23ca05b
Jonathan Coustick <jcoustick@sleightholme.com>
2026-03-11 14:30:55 +0000

    Reset compilation files to working version

--- Compile_Converted_Serverset.sh
+++ Compile_Converted_Serverset.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 echo 'pak192.comic open-source repository Serverset compiler for Unix'
 echo -e '======================================================\n'
 
@@ -40,90 +38,80 @@ compile() {
     local size=${#size[@]}
 
     for dat in $3; do
+        # get directory where the dat file is located
+        local dir=$(dirname "$dat")
 
-        if [ -f "$dat" ] ; then
-            echo -e "Compiling $dat"
-            ./makeobj-extended pak$1 ./compiled_serverset/ "./$dat" &> /dev/null
-                if [[ $? != 0 ]]; then
-                    echo "Error: Makeobj-extended returned an error for $dat. Aborting..."
-                fi
+        # hash the dat file
+        local IFS=' '
+        local dathash=($(sha256sum "$dat"))
+        local dathash=$dathash
+        local IFS=,
+
+        # obtain all image names inside the dat file
+        local images=$(awk -F= '
+            BEGIN {
+                IGNORECASE=1
+            }
+            {
+                if (/^([^#]*image\[|cursor|icon)/) {
+                    match($2, /(\.+[\/\\])*[a-z0-9\/\-_\\()]+/);
+                    images[substr($2, RSTART, RLENGTH)]++
+                }
+            }
+            END {
+                for (img in images) {
+                    if (img != "-") {
+                        printf "'$dir'/%s.png,", img
+                    }
+                }
+            }' "$dat")
+
+        if [[ ! -z $images ]]; then
+            # hash all the images
+            tempsha=$(sha256sum $images)
+
+            if [[ $? != 0 ]]; then
+                echo -e "\x1B[33mWarning: Failed to get one or more hashes on $dat\x1B[0m"
+            fi
+
+            local imghash=($(printf '%s %s\n' $tempsha | awk '{ printf "%s,", $1 }'))
         fi
 
-        # get directory where the dat file is located
-        #    local dir=$(dirname "$dat")
+        # get the hashes from the previous run
+        local validate=($(awk -F, "\$1 == \"$dat\"" "$csv"))
 
-            # hash the dat file
-        #    local IFS=' '
-         #   local dathash=($(sha256sum "$dat"))
-          #  local dathash=$dathash
-           # local IFS=,
+        # assume no recompilation necessary
+        local recompile=0
 
-            # obtain all image names inside the dat file
-           # local images=$(awk -F= '
-            #    BEGIN {
-             #       IGNORECASE=1
-              #  }
-               # {
-                #    if (/^([^#]*image\[|cursor|icon)/) {
-                 #       match($2, /(\.+[\/\\])*[a-z0-9\/\-_\\()]+/);
-                  #      images[substr($2, RSTART, RLENGTH)]++
-        #            }
-         #       }
-          #      END {
-           #         for (img in images) {
-            #            if (img != "-") {
-             #               printf "'$dir'/%s.png,", img
-              #          }
-               #     }
-   #             }' "$dat")
-#
- #           if [[ ! -z $images ]]; then
-  #              # hash all the images
-   #             tempsha=$(sha256sum $images)
-#
- #               if [[ $? != 0 ]]; then
-  #                  echo -e "\x1B[33mWarning: Failed to get one or more hashes on $dat\x1B[0m"
-   #             fi
-#
- #               local imghash=($(printf '%s %s\n' $tempsha | awk '{ printf "%s,", $1 }'))
-  #          fi
-#
- #           # get the hashes from the previous run
-  #          local validate=($(awk -F, "\$1 == \"$dat\"" "$csv"))
-#
- #           # assume no recompilation necessary
-  #          local recompile=0
-#
- #           # check hashes and number of images
-  #          if [[ $dathash != ${validate[1]} || $((${#validate[*]} - 2)) != ${#imghash[*]} ]]; then
-   #             local recompile=1
-    #        else
-     #           # check all image hashes
-      #          for hash in ${imghash[*]}; do
-       #             if [[ ! "${validate[*]}" =~ $hash ]]; then
-        #                local recompile=1
-         #           fi
-          #      done
-           # fi
-#
- #           # recompiling if necessary
-  #          if [[ $recompile == 1 ]]; then
-   #             
-    #       set -e o pipefail
-    #                echo -e "\x1B[33mError: Can not compile $dat\x1B[0m"
-    #                rm "$csv.in"
-    #                exit $?
-    #            fi
-    #        fi
+        # check hashes and number of images
+        if [[ $dathash != ${validate[1]} || $((${#validate[*]} - 2)) != ${#imghash[*]} ]]; then
+            local recompile=1
+        else
+            # check all image hashes
+            for hash in ${imghash[*]}; do
+                if [[ ! "${validate[*]}" =~ $hash ]]; then
+                    local recompile=1
+                fi
+            done
+        fi
 
-    #        # put the hashes in the $csv.in file
-     #       echo "$dat,$dathash,${imghash[*]}" >> "$csv.in"
+        # recompiling if necessary
+        if [[ $recompile == 1 ]]; then
+            ./makeobj-extended pak$1 ./compiled_serverset/ "./$dat" &> /dev/null
+            if [[ $? != 0 ]]; then
+                echo "Error: makeobj-extended returned an error for $dat. Aborting..."
+		set -e o pipefail
+                echo -e "\x1B[33mError: Can not compile $dat\x1B[0m"
+                rm "$csv.in"
+                exit $?
+            fi
+        fi
 
-            #if [ $TERM ] ; then
-            #    progressbar $index $size $dat
-            #fi
-      #      local index=$(( $index + 1 ))
-      #  fi
+        # put the hashes in the $csv.in file
+        echo "$dat,$dathash,${imghash[*]}" >> "$csv.in"
+
+        progressbar $index $size $dat
+        local index=$(( $index + 1 ))
     done
 
     # jump line because of progress bar
@@ -165,23 +153,12 @@ compile '64' 'User Interface' 'calculated/pakset/UI/64/*.dat'
 compile '128' 'User Interface' 'calculated/pakset/UI/128/*.dat'
 compile '192' 'User Interface' 'calculated/pakset/UI/192/*.dat'
 compile '384' 'Larger Objects' 'calculated/pakset/384/**/*.dat'
-compile '48' 'Smaller Objects' 'calculated/pakset/48/**/**/*.dat'
 
-compile '192' 'AddOns 1/15' 'calculated/AddOn/*.dat'
-compile '192' 'AddOns 2/15' 'calculated/AddOn/**/*.dat'
-compile '192' 'AddOns 3/15' 'calculated/AddOn/**/**/*.dat'
-compile '192' 'AddOns 4/15' 'calculated/AddOn/**/**/**/*.dat'
-compile '192' 'AddOns 5/15' 'calculated/AddOn/**/**/**/**/*.dat'
-compile '384' 'AddOns 6/15' 'calculated/AddOn384/*.dat'
-compile '384' 'AddOns 7/15' 'calculated/AddOn384/**/*.dat'
-compile '384' 'AddOns 8/15' 'calculated/AddOn384/**/**/*.dat'
-compile '384' 'AddOns 9/15' 'calculated/AddOn384/**/**/**/*.dat'
-compile '384' 'AddOns 10/15' 'calculated/AddOn384/**/**/**/**/*.dat'
-compile '48' 'AddOns 11/15' 'calculated/AddOn48/*.dat'
-compile '48' 'AddOns 12/15' 'calculated/AddOn48/**/*.dat'
-compile '48' 'AddOns 13/15' 'calculated/AddOn48/**/**/*.dat'
-compile '48' 'AddOns 14/15' 'calculated/AddOn48/**/**/**/*.dat'
-compile '48' 'AddOns 15/15' 'calculated/AddOn48/**/**/**/**/*.dat'
+compile '192' 'AddOns 1/5' 'calculated/AddOn/*.dat'
+compile '192' 'AddOns 2/5' 'calculated/AddOn/**/*.dat'
+compile '192' 'AddOns 3/5' 'calculated/AddOn/**/**/*.dat'
+compile '192' 'AddOns 4/5' 'calculated/AddOn/**/**/**/*.dat'
+compile '192' 'AddOns 5/5' 'calculated/AddOn/**/**/**/**/*.dat'
 
 #compile '192' 'British Stuff 1/2' 'calculated/AddOn/britain/**/*.dat'
 #compile '192' 'British Stuff 2/2' 'calculated/AddOn/britain/**/**/*.dat'
